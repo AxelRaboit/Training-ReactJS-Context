102b09991a24ee817ff21fd291a4df7fba7b7430
Axel Raboit <axel.raboit@gmail.com>
2022-02-01 14:57:47 +0100

    add comment about another possibility to render the context

--- src/components/MyContext.js
+++ src/components/MyContext.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+//De cette façon, le composant appelé dans le fichier ProfileData va afficher les datas donné ici par default à savoir Bart au lieu de Lisa
+/* const user = {
+	name: 'bart',
+	age: 9
+}
+
+const MyContext = React.createContext(user); */
+
 const MyContext = React.createContext();
 
 export { MyContext }
